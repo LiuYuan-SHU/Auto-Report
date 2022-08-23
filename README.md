40ca68b330c244ccb3ef440bd252b95445b186eb
Liuyuan <2787141886@qq.com>
2022-08-23 18:23:58 +0800

    Auto Report Ver.1

---
+++ README.md
@@ -0,0 +1,70 @@
+# AutoReport
+
+> 基于Selenium实现的SHU每日一报脚本
+
+## quick start
+
+### 安装依赖
+
+#### Python
+
+船新版本：[Python 3.10.6](https://www.python.org/downloads/release/python-3106/)
+
+#### Selenium
+
+```sh
+python -m pip install -i https://pypi.doubanio.com/simple selenium
+```
+
+#### WebDriver for Chrome
+
+首先需要安装Chrome浏览器，然后[下载Chrome驱动](https://chromedriver.chromium.org/downloads)。在下载之前，需要检查自己的Chrome的浏览器版本，然后再选择对应的版本安装
+
+<div align="center">
+	<img src="README.assets/image-20220823173442755.png" alt="image-20220823173442755" style="zoom:50%;" />
+	<img src="README.assets/image-20220823173510139.png" alt="image-20220823173510139" style="zoom:50%;" />
+	<img src="README.assets/image-20220823173531918.png" alt="image-20220823173531918" style="zoom:50%;" />
+</div>
+
+安装完之后，需要将下载好的压缩包解压，然后将可执行文件添加到系统环境变量中
+
+### 配置data.json
+
+> 程序不对本文件的正确性进行判断，所以请不要修改文件的格式，也不要将字段值设定在范围之外
+
+根据所在地的不同，本文件的一些内容需要修改：
+
+#### `in_Shanghai_index`
+
+1. 如果在上海，且当日进过学校，请将本字段设置为1
+	+ 根据当日所在的校区，设置`zone`
+	+ 根据当日是否住校，设置`campus`
+2. 如果在上海，但是当日没有进过学校，请将本字段设置为2
+3. 如果不在上海，请将本字段设置为3
+
+#### `is_home_address`
+
+每日一报系统默认采用之前的数据填写结果作为默认地址，因此你需要对这个默认地址进行判断：是否是家庭地址。如果是家庭地址，那么请将本字段设置为1，否则请设置为2
+
+### 运行程序
+
+在运行程序之前，你需要检查自己是否填写过每日一报。意即，如果近一个月你都没有填写过每日一报，那么需要你先填写一天，然后再开始运行程序
+
+```sh
+python3 ./work.py
+```
+
+## 项目结构
+
+```mermaid
+graph LR;
+main --> login --> get_item_list --iterate_item--> submit_info   
+```
+
++ `login`函数负责登录，如果登录超时（10s）或者密码错误都会导致程序结束
++ `get_item_list`获取没有填写的每日一报，并且返回一个列表
++ `submit_info`填写数据并提交
+
+## 存在的一些问题
+
+在`login`函数中，用异常处理来判断密码是否正确，导致代码整个看起来非常抽象。但是也没有找到什么好的解决办法。
---
+++ work.py
@@ -0,0 +1,158 @@
+# Selenium组件
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+# 标准库组件
+import logging          # 日志
+import json             # JSON文件操作
+import os               # 判断文件是否存在
+import sys              # quit
+import re               # 正则表达式
+from time import sleep  # 程序等待
+
+# logging configuration
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s: %(message)s')
+
+# selenium configuration
+# headless mode
+options = ChromeOptions()
+options.add_argument('--headless')
+# browser
+browser = webdriver.Chrome(options=options)
+browser.set_window_size(1920, 1080)
+
+# 每日一报URL
+report_history_url = 'https://selfreport.shu.edu.cn/ReportHistory.aspx'
+
+# 登录
+# 登录界面需要在10s之内加载完毕，否则会抛出TimeoutException
+# 如果出现这样的情况，提醒检查网络状态并退出程序
+# 如果输入了错误的账号和密码，那么也会退出程序
+def login(username, password):
+    try:
+        browser.get(report_history_url)
+        wait = WebDriverWait(browser, 10)
+        # get input element to input username
+        input_username = wait.until(EC.presence_of_element_located((By.XPATH, './/input[@id="username"]')))
+        # get input element to input password
+        input_password = browser.find_element(By.XPATH, './/input[@id="password"]')
+        # get button element
+        button_login = browser.find_element(By.CLASS_NAME, 'submit-button')
+        input_username.send_keys(username)
+        input_password.send_keys(password)
+        button_login.click()
+    except TimeoutException:
+        logging.error('requests time out. please check your network status')
+        sys.exit()
+
+    # wrong password or ID
+    try:
+        button_wrong_password = browser.find_element(By.XPATH, './/button[contains(text(), "ok")]')
+        logging.error('wrong password, exit')
+        sys.exit()
+    except NoSuchElementException:
+        logging.info('login successfully')        
+
+    return
+
+# return reversed datalist
+# 需要注意的是，如果超时，那么就会默认所有每日一报已经完成，那么就会结束程序
+def get_item_list():
+    logging.info('scraping reports list')
+    item_list = []
+
+    try:
+        wait = WebDriverWait(browser, 3)
+        item_list = wait.until(EC.presence_of_all_elements_located((By.XPATH, './/a[@class="f-datalist-item-inner" and contains(text(), "未填报")]')))
+    except TimeoutException:
+        logging.info('all reports submitted, exit')
+        sys.exit()
+
+    return item_list[::-1]
+
+# get WebElement
+# 根据传入的XPath返回元素
+def get_element(xpath):
+    return browser.find_element(By.XPATH, xpath)
+
+# 填写每日一报
+def submit_info():
+    # 检索文件data.json是否存在
+    if os.path.exists('./data.json'):
+        json_data = json.load(open('data.json', encoding='utf-8')) 
+    else:
+        logging.error('json file data.json not found, fatal error, exit')
+        sys.exit()
+
+    wait = WebDriverWait(browser, 10)
+    # promise button
+    button_promise = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/form/div[5]/div/div[2]/div[1]/div[1]/div[2]/div/div/i')))
+    if button_promise.get_attribute('class').find('f-checked') == -1:
+        button_promise.click()
+
+    # in_Shanghai
+    in_Shanghai_index = json_data["in_Shanghai_index"]
+    button_in_Shanghai = get_element('/html/body/form/div[5]/div/div[2]/div[1]/div[9]/div/div/div[2]/div[2]/div/table/tr[{index}]/td/div/div[2]/div/div/i'.format(index = in_Shanghai_index))
+    if button_in_Shanghai.get_attribute('class').find('f-checked') == -1:
+        button_in_Shanghai.click()
+    
+    # special case handling
+    if in_Shanghai_index == 1:
+        button_zone = get_element('/html/body/form/div[5]/div/div[2]/div[1]/div[9]/div/div/div[3]/div[2]/div/table/tr/td[{index}]/div/div[2]/div/div/i'.format(index = json_data["in_Shanghai"].get('zone'))) 
+        if button_zone.get_attribute('class').find('f-checked') == -1:
+            button_zone.click()
+        button_campus = get_element('/html/body/form/div[5]/div/div[2]/div[1]/div[9]/div/div/div[4]/div[2]/div/table/tr/td[{index}]/div/div[2]/div/div/i'.format(index = json_data["in_Shanghai"].get('campus'))) 
+        if button_campus.get_attribute('class').find('f-checked') == -1:
+            button_campus.click()
+
+    # is_home
+    button_is_home = get_element('/html/body/form/div[5]/div/div[2]/div[1]/div[28]/div[2]/div/table/tr/td[{index}]/div/div[2]/div/div/i'.format(index = json_data["is_home_address"]))
+    if button_is_home.get_attribute('class').find('f-checked') == -1:
+        button_is_home.click()
+
+    # high risk zone
+    button_high_risk = get_element('/html/body/form/div[5]/div/div[2]/div[1]/div[29]/div[2]/div/table/tr[1]/td/div/div[2]/div/div/i')
+    if button_high_risk.get_attribute('class').find('f-checked') == -1:
+        button_high_risk.click()
+    # touch
+    button_touch = get_element('/html/body/form/div[5]/div/div[2]/div[1]/div[30]/div[2]/div/table/tr/td[2]/div/div[2]/div/div/i')
+    if button_touch.get_attribute('class').find('f-checked') == -1:
+        button_touch.click()
+    # isolate
+    button_isolate = get_element('/html/body/form/div[5]/div/div[2]/div[1]/div[31]/div[2]/div/table/tr/td[2]/div/div[2]/div/div/i')
+    if button_isolate.get_attribute('class').find('f-checked') == -1:
+        button_isolate.click()
+
+    # submit
+    button_submit = get_element('/html/body/form/div[5]/div/div[2]/div[2]/div/div/a[1]')
+    button_submit.click()
+
+    button_YES = wait.until(EC.presence_of_element_located((By.ID, 'fineui_39')))
+    button_YES.click()
+    # 防止点击过快造成的表单提交失败
+    sleep(1.5)
+
+def main():
+    username = input('please input your student ID in SHU: ')
+    password = input("please input your student password in SHU(Don't worry, the program won't record your ID & Password):")
+    login(username, password)
+
+    # get datalist
+    item_list = get_item_list()
+
+    for item in item_list:
+        browser.get(item.get_attribute('href'))
+        submit_info()
+        browser.back()
+        logging.info('finished item with date ' + re.search('(\d{4}-\d{2}-\d{2})', item.text).group(0))
+
+if __name__ == '__main__':
+    try:
+        main()
+        logging.info('all reportsfinished, exit')
+    finally:
+        browser.close()
